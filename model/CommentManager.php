d1d7bce1bd3996e46a524bd8ef88522fbf5d25a3
david <david.ravel@yahoo.fr>
2018-08-06 15:56:46 +0200

    Creation du fichier CommentManager.php avec la fonction getComments

---
+++ model/CommentManager.php
@@ -0,0 +1,15 @@
+<?php
+require_once('model/Manager.php');
+
+class CommentManager extends Manager
+{
+    public function getComments($postId)
+    {
+        $db = $this->dbConnect();
+        $comments = $db->prepare('SELECT id, post_id, author, comment,report, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments->execute(array($postId));
+
+        return $comments;
+    }
+
+}
