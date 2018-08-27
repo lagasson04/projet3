e7ad3f48966d07076819078426e2ed694cc6f4be
david <david.ravel@yahoo.fr>
2018-08-27 18:48:20 +0200

    Modification indentation fichier listPostsView.php

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -6,33 +6,33 @@
 <?php 
 while ($data = $posts->fetch()) 
 {
-?>
-<div class="container">
-	<br>
-	<div class="row">
-		<div class="col-md-12">
-			<div class="card">
-				<div class="card-header">
-					<h3>
-						<?= strip_tags($data['title']) ?>
-					</h3>
-				</div>
-				<div class="card-body">
-					<p>
-						<!-- On affiche le contenu du billet -->
-						<?= nl2br(strip_tags($data['extractString'])) ?><a class="readmore" href="index.php?action=post&amp;id=<?= $data['id'] ?>"><br>...Lire plus</a>
-						<em><strong>le <?= $data['creation_date_fr'] ?></strong></em>
-						<p><em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em></p>
-						<hr>
-					</p>
+	?>
+	<div class="container">
+		<br>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-header">
+						<h3>
+							<?= strip_tags($data['title']) ?>
+						</h3>
+					</div>
+					<div class="card-body">
+						<p>
+							<!-- On affiche le contenu du billet -->
+							<?= nl2br(strip_tags($data['extractString'])) ?><a class="readmore" href="index.php?action=post&amp;id=<?= $data['id'] ?>"><br>...Lire plus</a>
+							<em><strong>le <?= $data['creation_date_fr'] ?></strong></em>
+							<p><em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em></p>
+							<hr>
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<?php
+	<?php
 }
-$posts->closeCursor(); 
-?>
+$posts->closeCursor();
+?> 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php');
